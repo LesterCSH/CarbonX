e9b13b57a5a1828f253cb6e102ad750db3ec9d88
alvan1009 <138870989+alvan1009@users.noreply.github.com>
2023-12-25 16:11:45 +0800

    Update Welcome.jsx

--- react_front-end/client/src/components/Welcome.jsx
+++ react_front-end/client/src/components/Welcome.jsx
@@ -92,7 +92,9 @@ const Welcome = () => {
             </div>
           </div>
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            <Input placeholder="Amount (Carbon)" name="amount" type="number" handleChange={handleChange} />
+            <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
             <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
